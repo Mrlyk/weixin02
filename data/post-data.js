16fdf0314fc1a3fd5002baaef41d40f196bec544
Mrlyk <1065923082@qq.com>
2018-07-05 16:12:32 +0800

    数据引用和模板

---
+++ data/post-data.js
@@ -0,0 +1,43 @@
+// 视为本地数据库
+var haimian = [{
+  date: "July 12 2017",
+  title: "海绵宝宝1",
+  content: "《海绵宝宝》（SpongeBob SquarePants）是一部由舍曼·科恩、沃特·杜赫、山姆·亨德森、保罗·蒂比特等导演，汤姆·肯尼、比尔·法格巴克、罗杰·布帕斯等配音的美国喜剧动画，于1999年7月17日在尼克国际儿童频道开播",
+  img: {
+    imgtouxiang: "/images/touxiang/touxiang1.png",
+    imgartical: "/images/artical/01.png",
+  },
+  shou: "78",
+  zan: "187"
+},
+
+{
+  date: "April 5 2018",
+  title: "海绵宝宝2",
+  content: "《海绵宝宝》的故事情节主要围绕着主角海绵宝宝和他的好朋友派大星、邻居章鱼哥、上司蟹老板等人展开，场景设定于太平洋海底，一座被称为比奇堡的城市.2005年1月30日，该片荣获第32届安妮奖授予的“最佳TV动画制作”奖",
+  img: {
+    imgtouxiang: "/images/touxiang/touxiang2.png",
+    imgartical: "/images/artical/02.png",
+  },
+  shou: "87",
+  zan: "198"
+},
+
+{
+  date: "April 13 2018",
+  title: "海绵宝宝3",
+  content: "这部动画除了固定描绘的卡通场景与人物之外，也会穿插一些真实的物件或人物：例如曾出演《海滩游侠》与《霹雳游侠》的大卫·哈塞尔霍夫，以本人的身份出演了几集。但海绵宝宝卡通的内容基本上与海洋知识无关，甚至夸大到完全不合乎科学与常识，例如海底生火、海底冲澡等，剧集内容也会时不时的嘲笑精致艺术和章鱼哥的劳工权益想法。",
+  img: {
+    imgtouxiang: "/images/touxiang/touxiang3.png",
+    imgartical: "/images/artical/03.png",
+  },
+  shou: "70",
+  zan: "159"
+}
+]
+
+// 出口,可添加多个
+module.exports={
+  postlist:haimian
+
+}
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,3 +1,6 @@
+// 用来接收输出的对象require来获得
+var postdata =require("../../data/post-data.js")//rquire后面是路径地址，但是只能用相对路径，不能用绝对路径
+
 Page({
 
   /**
@@ -6,7 +9,7 @@ Page({
   data: {
     // date2: "April 5 2018",
     /**数据绑定，另外一边可以直接引用 */
-    haimian_key: []
+    // haimian_key: []
 
 
   },
@@ -27,45 +30,11 @@ Page({
     // console.log("onLoad");
 
     /**假设下面的数据是从服务器获取到的 */
-    var haimian = [{
-        date: "July 12 2017",
-        title: "海绵宝宝1",
-        content: "《海绵宝宝》（SpongeBob SquarePants）是一部由舍曼·科恩、沃特·杜赫、山姆·亨德森、保罗·蒂比特等导演，汤姆·肯尼、比尔·法格巴克、罗杰·布帕斯等配音的美国喜剧动画，于1999年7月17日在尼克国际儿童频道开播",
-        img: {
-          imgtouxiang: "/images/touxiang/touxiang1.png",
-          img: "/images/artical/01.png",
-        },
-        shou: "78",
-        zan: "187"
-      },
 
-      {
-        date: "April 5 2018",
-        title: "海绵宝宝2",
-        content: "《海绵宝宝》的故事情节主要围绕着主角海绵宝宝和他的好朋友派大星、邻居章鱼哥、上司蟹老板等人展开，场景设定于太平洋海底，一座被称为比奇堡的城市.2005年1月30日，该片荣获第32届安妮奖授予的“最佳TV动画制作”奖",
-        img: {
-          imgtouxiang: "/images/touxiang/touxiang2.png",
-          img: "/images/artical/02.png",
-        },
-        shou: "87",
-        zan: "198"
-      },
-      
-      {
-        date: "April 13 2018",
-        title: "海绵宝宝3",
-        content: "这部动画除了固定描绘的卡通场景与人物之外，也会穿插一些真实的物件或人物：例如曾出演《海滩游侠》与《霹雳游侠》的大卫·哈塞尔霍夫，以本人的身份出演了几集。但海绵宝宝卡通的内容基本上与海洋知识无关，甚至夸大到完全不合乎科学与常识，例如海底生火、海底冲澡等，剧集内容也会时不时的嘲笑精致艺术和章鱼哥的劳工权益想法。",
-        img: {
-          imgtouxiang: "/images/touxiang/touxiang3.png",
-          img: "/images/artical/03.png",
-        },
-        shou: "70",
-        zan: "159"
-      }
-    ]
-
+// this.setData来更新上方data里的数据来实现数据绑定
+//小程序总是会读取data对象来做数绑定，而这个动作是在onload执行之后才发生的。
     this.setData({
-      haimian_key: haimian
+      haimian_key: postdata.postlist
     });
 
 
